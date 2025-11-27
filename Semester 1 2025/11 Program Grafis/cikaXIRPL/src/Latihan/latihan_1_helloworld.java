2f38b50340e25ecbb337dcf82158df7567b92451
Leon Kai <muhfadelramadan06@gmail.com>
2025-11-27 13:08:11 +0800

    u

--- Semester 1 2025/11 Program Grafis/cikaXIRPL/src/Latihan/latihan_1_helloworld.java
+++
@@ -1,8 +0,0 @@
-package latihan;
-
-
-public class latihan_1_helloworld {
-    public static void main (String[] args){
-        System.out.print ("Hello World");
-    }
-}
--- Semester 1 2025/11 Program Grafis/cikaXIRPL/src/soal/tugas_2_nama_variabel.java
+++
@@ -1,8 +0,0 @@
-package soal;
-
-public class tugas_2_nama_variabel {
-    public static void main(String[] ayam){
-        String Namasiswa ="pida,asyraf,melinda,zamir,caca,naufal cika";
-        System.out.println(Namasiswa +  "Kelas XI.RPL");
-    }
-  }
